14af618b3b14752c6d7e0ce731cc00504299f73b
lagoa <lagoas.joaoluiz@gmail.com>
2025-10-04 12:28:11 -0300

    Carrega tabuleiro de Dama inicialmente.

--- WinFormsApp3/Program.cs
+++ WinFormsApp3/Program.cs
@@ -1,3 +1,5 @@
+using WinFormsApp3.Forms;
+
 namespace WinFormsApp3
 {
     internal static class Program
@@ -11,7 +13,7 @@ namespace WinFormsApp3
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new SingInForm1());
+            Application.Run(new DamasForms());
         }
     }
 }
